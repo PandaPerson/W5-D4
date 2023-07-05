b037bbcb1546682bbe6955ecf4f3c54900226001
PandaPerson <91638652+PandaPerson@users.noreply.github.com>
2023-07-05 16:27:11 -0600

    removed alert from app.js

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ $('.navItem').css('color', '#FFF');
 $('#nav').css('background-color', '#145');
 $('li:even').css('background-color', '#ccc');
 $('li:odd').css('background-color', '#aaa');
-alert($('input').val('Drew'));
+$('input').val('Drew');
